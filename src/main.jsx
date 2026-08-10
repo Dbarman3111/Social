4109f1dec90c6ff8845e1cf7c239ca89b7025954
Dipankar Barman <dipankarbarman803@gmail.com>
2026-08-10 18:59:49 +0530

    Comment out StrictMode in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,11 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <GoogleOAuthProvider clientId={googleClientId}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </GoogleOAuthProvider>
-  </StrictMode>,
+  // </StrictMode>,
 )
